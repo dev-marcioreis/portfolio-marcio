7967b1efc3a96e4c87a4b8deca579ede5e07919e
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-07-13 11:36:17 -0300

    Feat: Swiper slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-let swiperProjects = new Swiper('.projects__container', {
-    loop: true,
-    spaceBetween: 24,
-    mousewhel: true,
-    Keyboard: true
-})
+let swiperProjects = new Swiper(".projects__container", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+        992: {
+          slidesPerView: 2,
+          spaceBetween: -20,
+        },
+      },
+  });
