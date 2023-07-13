b93f67c95cc3f1a696a91c95dad6c4ef5ce11595
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-07-13 17:12:32 -0300

    Feat: recente section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Swiper
 let swiperProjects = new Swiper(".projects__container", {
     cssMode: true,
     navigation: {
@@ -14,3 +15,97 @@ let swiperProjects = new Swiper(".projects__container", {
         },
       },
   });
+
+
+// Scroll Up
+const scrollUp = () => {
+  const scrollUp = document.querySelector('.scroll__up')
+
+  if(this.scrollY >= 200) {
+      scrollUp.classList.add('show-scroll')
+  } else {
+      scrollUp.classList.remove('show-scroll')
+  }
+}
+
+window.addEventListener('scroll', scrollUp)
+
+
+// Portfolio
+const eye = document.querySelector('.portfolio__eye')
+const back = document.querySelector('.btn__back')
+
+let tOne = gsap.timeline( {paused: true, reversed: true} )
+let tTwo = gsap.timeline( {paused: true, reversed: true} )
+
+eye.addEventListener('click', () => {
+  tTwo.play()
+})
+
+back.addEventListener('click', () => {
+  tTwo.reverse()
+})
+
+tOne.play()
+
+tOne.from('.btn__back', {
+    ease: "elastic.out(1, .8)",
+    scale: 0,
+    duration: 1.5
+}, 0.6)
+
+tTwo.to('.portfolio__image', {
+  ease: 'elastic.out(1, .8)',
+  top: '-60%',
+  duration: .1,
+})
+
+tTwo.to('.portfolio__title', {
+  ease: 'elastic.out(.5, .8)',
+  bottom: '-25%'
+}, .1)
+
+tTwo.to('.portfolio__eye i', {
+  ease: 'power1.inOut',
+  scale: 0,
+  duration: .1,
+}, .1)
+
+tTwo.to('.portfolio__eye', {
+  ease: 'power1.inOut',
+  scaleY: 20,
+  width: 300,
+  duration: 0.1,
+}, 0.1)
+
+
+tTwo.to('.portfolio__image-1', {
+  ease: 'elastic.out(1, .8)',
+  top: '0%',
+  duration: 1.5,
+}, 0.5)
+
+tTwo.to('.layer__title', {
+  ease: 'elastic.out(1, .8)',
+  left: '60%',
+  duration: 1.5,
+}, 0.5)
+
+tTwo.to('.portfolio__sumary-1', {
+  ease: 'elastic.out(1, .8)',
+  top: '53%',
+  duration: 1.5,
+}, 0.5)
+
+tTwo.to('.portfolio__sumary-2', {
+  ease: 'elastic.out(1, .8)',
+  top: '60%',
+  duration: 1.5,
+}, 0.5)
+
+tTwo.to('.portfolio__btns', {
+  ease: 'elastic.out(1, .8)',
+  bottom: '3%',
+  duration: 1.5,
+}, 0.5)
+
